2d660a53447c53915eeb8ce2ee10437dcaea9cff
codydecoder <142354876+codydecoder@users.noreply.github.com>
2024-02-02 08:10:17 -0600

    Final Commit before submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const recApiKey = `Pp3wBK5nbXNBnkeA5NQT3XgcxEJ7jD1Q02IE23Cq`
 
 let button = document.querySelector("#submitButton")
 
-let apiLimit = 52;
+let apiLimit = 50;
 let apiStart = 0;
 
 let parkStorage = [];
@@ -15,6 +15,8 @@ button.addEventListener('click', async () => {
    parkListContainer.innerHTML = ""
 
    let apiQuery = textInput.value;
+
+    let apiCall = 0;
 
    let response = await axios.get(`https://developer.nps.gov/api/v1/parks?limit=50&start=0&q=${apiQuery}&api_key=${recApiKey}`)
    for(let i = 0; i < response.data.data.length; i++) {
@@ -32,7 +34,7 @@ button.addEventListener('click', async () => {
    
          const newPark = document.createElement('li')
          parkListContainer.appendChild(newPark)
-         newPark.innerHTML = `<span class="cursive">Park Name :  </span>${response.data.data[a].fullName}</br> <span class="images"><img src="${response.data.data[a].images[0].url}"></span></br><span class="cursive">Hours: </span> ${response.data.data[a].operatingHours[0].standardHours.monday}`
+         /**/newPark.innerHTML = `<span class="cursive">Park Name :  </span>${response.data.data[a].fullName}</br> <img class="apiImages" src="${response.data.data[a].images[0].url}"></br><span class="cursive">Hours: </span> ${response.data.data[a].operatingHours[0].standardHours.wednesday}</br><span class="cursive">Address: </span>${response.data.data[a].addresses[0].line1}, ${response.data.data[a].addresses[0].city}, ${response.data.data[a].addresses[0].stateCode} ${response.data.data[a].addresses[0].postalCode}</br><span class="cursive">Website: </span><a href="${response.data.data[a].url}">${response.data.data[a].url}</a></br><span class="cursive">Phone Number: </span>${response.data.data[a].contacts.phoneNumbers.length > 0 ? response.data.data[a].contacts.phoneNumbers[0].phoneNumber : "None available"}`
          
       
       
@@ -51,5 +53,6 @@ button.addEventListener('click', async () => {
       
    }
 
-
+   apiCall = (apiCall + 1)
 })
+
